228799c3641e611dabb999624f030754e8ce6dca
auxo86 <auxo86@gmail.com>
2017-06-01 18:11:26 +0800

    09-04

---
+++ Mod09_01/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mod09_01.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Home/Index2
+        public ActionResult Index2()
+        {
+            return View();
+        }
+
+        // GET: Home/SelectorDemo
+        public ActionResult SelectorDemo()
+        {
+            return View();
+        }
+
+        // GET: Home/HoverDemo
+        public ActionResult HoverDemo()
+        {
+            return View();
+        }
+
+
+    }
+}
